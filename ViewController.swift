cd1b2c51d492b28317a6d154714741ef184527fd
nika <nikakalandias@gmail.com>
2022-02-07 18:01:23 +0300

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView-App
+//
+//  Created by Nika Kalandia on 7.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
