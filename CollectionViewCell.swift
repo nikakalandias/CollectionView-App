e9b56ba3fde36f602792f9c539512db6fd624c17
nika <nikakalandias@gmail.com>
2022-02-08 00:48:56 +0300

    Functions & Cell

---
+++ CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  CollectionView-App
+//
+//  Created by Nika Kalandia on 7.02.2022.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var testLbl: UILabel!
+    
+}
---
+++ CollectionViewController.swift
@@ -0,0 +1,74 @@
+//
+//  CollectionViewController.swift
+//  CollectionView-App
+//
+//  Created by Nika Kalandia on 7.02.2022.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class CollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        
+        navBarAyarla(largeTitleColor: .white, backgoundColor: .darkGray, tintColor: .white, title: "Test Title", preferredLargeTitle: true)
+    }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 21
+    }
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        
+        if let customCell = cell as? CollectionViewCell {
+            customCell.testLbl.text = "\(indexPath.row)"
+            return customCell
+        }
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("tapped")
+    }
+    
+    
+    //============================================================================
+    func navBarAyarla(largeTitleColor: UIColor, backgoundColor: UIColor, tintColor: UIColor, title: String, preferredLargeTitle: Bool) {
+        
+    if #available(iOS 13.0, *) { // iOS 13 ve sonrası için
+    let navBarAppearance = UINavigationBarAppearance()
+    navBarAppearance.configureWithOpaqueBackground()
+    navBarAppearance.largeTitleTextAttributes = [.foregroundColor: largeTitleColor]
+    navBarAppearance.titleTextAttributes = [.foregroundColor: largeTitleColor]
+    navBarAppearance.backgroundColor = backgoundColor
+     
+    navigationController?.navigationBar.standardAppearance = navBarAppearance
+    navigationController?.navigationBar.compactAppearance = navBarAppearance
+    navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+     
+    navigationController?.navigationBar.prefersLargeTitles = preferredLargeTitle
+    navigationController?.navigationBar.isTranslucent = false
+    navigationController?.navigationBar.tintColor = tintColor
+    navigationItem.title = title
+     
+    } else { // iOS 13 ve öncesi için
+     
+    navigationController?.navigationBar.barTintColor = backgoundColor
+    navigationController?.navigationBar.tintColor = tintColor
+    navigationController?.navigationBar.isTranslucent = false
+    navigationItem.title = title
+    }
+    }
+    
+    
+}
